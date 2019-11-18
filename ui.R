8c621a231e009da33b9178ab3249ab42a4e7199d
anglissr <anglissr@uw.edu>
2019-11-18 11:47:17 -0800

    server and ui first tests

---
+++ ui.R
@@ -0,0 +1,92 @@
+# testing shiny
+
+library(shiny)
+
+
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  tags$head(
+    tags$title("testing"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "bootstrap.css"),
+    tags$meta(charset = "utf-8"),
+    tags$meta(name = "viewport", content = "width=device-width, initial-scale=1, shrink-to-fit=no")
+  ),
+
+  navbarPage(
+    id = "navbar",
+    collapsible = TRUE,
+    header = TRUE,
+    position = c("fixed-top"),
+    title = "Electric Vehicles",
+
+    tabPanel("Home", 
+                 
+    ),
+
+    tabPanel("Team",
+             tags$header(
+                 tags$div(id = "header",
+                          h1("The Team")
+                 )
+             ),
+             
+        tags$div(class = "row",
+                 tags$div(class = "column",
+                          tags$div(class = "card",
+                                   tags$img(src="pic.jpg", alt="", style="width:100%"),
+                                   tags$div(class = "container",
+                                            tags$h3("Davis Kurniawan"),
+                                            tags$p("Major: [Text Here]"),
+                                            tags$p(tags$button(class = "button", "Github"))
+                                            )
+                                   )
+                          ),
+                 
+                 tags$div(class = "column",
+                          tags$div(class = "card",
+                                   tags$img(src="pic.jpg", alt="", style="width:100%"),
+                                   tags$div(class = "container",
+                                            tags$h3("Jareese Espinosa"),
+                                            tags$p("Major: [Text Here]"),
+                                            tags$p(tags$button(class = "button", "Github"))
+                                            )
+                                    )
+                        ),
+                 
+                 tags$div(class = "column",
+                          tags$div(class = "card",
+                                   tags$img(src="ryanangliss.jpg", alt="", style="width:100%"),
+                                   tags$div(class = "container",
+                                            tags$h3("Ryan Angliss"),
+                                            tags$p("Major: [Text Here]"),
+                                            tags$p(tags$button(class = "button", "Github"))
+                                            )
+                                    )
+                        ),
+                 tags$div(class = "column",
+                          tags$div(class = "card",
+                                   tags$img(src="pic.jpg", alt="", style="width:100%"),
+                                   tags$div(class = "container",
+                                            tags$h3("Spencer Boat"),
+                                            tags$p("Major: [Text Here]"),
+                                            tags$p(tags$button(class = "button", tags$a(href="www.rstudio.com", "Github")))
+                                           )
+                                  )
+                         ),
+                 
+                 tags$div(class = "column",
+                          tags$div(class = "card",
+                                   tags$img(src="pic.jpg", alt="", style="width:100%"),
+                                   tags$div(class = "container",
+                                            tags$h3("Zhiyan Keriwn Jiao"),
+                                            tags$p("Major: [Text Here]"),
+                                            tags$p(tags$button(class = "button", "Github"))
+                                           )
+                                  )
+                         )
+                 )
+    )
+    
+  )
+))
