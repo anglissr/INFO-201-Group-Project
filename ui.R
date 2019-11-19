7320de744417c358085c05a6bb6458b5da90ec65
Spencer <spencer@theboatfamily.com>
2019-11-19 15:10:33 -0800

    made a plot

--- ui.R
+++ ui.R
@@ -9,17 +9,17 @@ shinyUI(fluidPage(
     tags$meta(charset = "utf-8"),
     tags$meta(name = "viewport", content = "width=device-width, initial-scale=1, shrink-to-fit=no")
   ),
-
+  
   navbarPage(
     id = "navbar",
     collapsible = TRUE,
     header = TRUE,
     position = c("fixed-top"),
     title = "Electric Vehicles",
-
+    
     
     tabPanel("Home", 
-               
+             
     ),
     
     tabPanel("Carbon Emmissions in the US", 
@@ -38,88 +38,88 @@ shinyUI(fluidPage(
              
     ),
     
-
+    
     
     tabPanel("Team",
-      
-      tags$header(
-        tags$div(
-          id = "header",
-          h1("The Team")
-        )
-      ),
-
-      tags$div(
-        class = "row",
-        
-        tags$div(
-          class = "column",
-          tags$div(
-            class = "card",
-            tags$img(src = "pic.jpg", alt = "", style = "width:100%"),
-            tags$div(
-              class = "container",
-              tags$h3("Davis Kurniawan"),
-              tags$p("Major: [Text Here]"),
-              tags$p(tags$button(class = "button", tags$a(href = "https://github.com/davisk83", "Github")))
-            )
-          )
-        ),
-
-        tags$div(
-          class = "column",
-          tags$div(
-            class = "card",
-            tags$img(src = "pic.jpg", alt = "", style = "width:100%"),
-            tags$div(
-              class = "container",
-              tags$h3("Jareese Espinosa"),
-              tags$p("Major: [Text Here]"),
-              tags$p(tags$button(class = "button", tags$a(href = "https://github.com/JareeseEspinosa", "Github")))
-            )
-          )
-        ),
-        tags$div(
-          class = "column",
-          tags$div(
-            class = "card",
-            tags$img(src = "ryanangliss.jpg", alt = "", style = "width:100%"),
-            tags$div(
-              class = "container",
-              tags$h3("Ryan Angliss"),
-              tags$p("Major: [Text Here]"),
-              tags$p(tags$button(class = "button", tags$a(href = "https://github.com/anglissr", "Github")))
-            )
-          )
-        ),
-        tags$div(
-          class = "column",
-          tags$div(
-            class = "card",
-            tags$img(src = "pic.jpg", alt = "", style = "width:100%"),
-            tags$div(
-              class = "container",
-              tags$h3("Spencer Boat"),
-              tags$p("Major: [Text Here]"),
-              tags$p(tags$button(class = "button", tags$a(href = "https://github.com/Nee-er", "Github")))
-            )
-          )
-        ),
-
-        tags$div(
-          class = "column",
-          tags$div(
-            class = "card",
-            tags$img(src = "pic.jpg", alt = "", style = "width:100%"),
-            tags$div(
-              class = "container",
-              tags$h3("Zhiyan Keriwn Jiao"),
-              tags$p("Major: [Text Here]"),
-              tags$p(tags$button(class = "button", tags$a(href = "https://github.com/Zhiyuan-Jiao", "Github")))
-            )
-          )
-        )
-      )
+             
+             tags$header(
+               tags$div(
+                 id = "header",
+                 h1("The Team")
+               )
+             ),
+             
+             tags$div(
+               class = "row",
+               
+               tags$div(
+                 class = "column",
+                 tags$div(
+                   class = "card",
+                   tags$img(src = "pic.jpg", alt = "", style = "width:100%"),
+                   tags$div(
+                     class = "container",
+                     tags$h3("Davis Kurniawan"),
+                     tags$p("Major: [Text Here]"),
+                     tags$p(tags$button(class = "button", tags$a(href = "https://github.com/davisk83", "Github")))
+                   )
+                 )
+               ),
+               
+               tags$div(
+                 class = "column",
+                 tags$div(
+                   class = "card",
+                   tags$img(src = "pic.jpg", alt = "", style = "width:100%"),
+                   tags$div(
+                     class = "container",
+                     tags$h3("Jareese Espinosa"),
+                     tags$p("Major: [Text Here]"),
+                     tags$p(tags$button(class = "button", tags$a(href = "https://github.com/JareeseEspinosa", "Github")))
+                   )
+                 )
+               ),
+               tags$div(
+                 class = "column",
+                 tags$div(
+                   class = "card",
+                   tags$img(src = "ryanangliss.jpg", alt = "", style = "width:100%"),
+                   tags$div(
+                     class = "container",
+                     tags$h3("Ryan Angliss"),
+                     tags$p("Major: [Text Here]"),
+                     tags$p(tags$button(class = "button", tags$a(href = "https://github.com/anglissr", "Github")))
+                   )
+                 )
+               ),
+               tags$div(
+                 class = "column",
+                 tags$div(
+                   class = "card",
+                   tags$img(src = "pic.jpg", alt = "", style = "width:100%"),
+                   tags$div(
+                     class = "container",
+                     tags$h3("Spencer Boat"),
+                     tags$p("Major: [Text Here]"),
+                     tags$p(tags$button(class = "button", tags$a(href = "https://github.com/Nee-er", "Github")))
+                   )
+                 )
+               ),
+               
+               tags$div(
+                 class = "column",
+                 tags$div(
+                   class = "card",
+                   tags$img(src = "pic.jpg", alt = "", style = "width:100%"),
+                   tags$div(
+                     class = "container",
+                     tags$h3("Zhiyan Keriwn Jiao"),
+                     tags$p("Major: [Text Here]"),
+                     tags$p(tags$button(class = "button", tags$a(href = "https://github.com/Zhiyuan-Jiao", "Github")))
+                   )
+                 )
+               )
+             )
     )
   )
 ))
